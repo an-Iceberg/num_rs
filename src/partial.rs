8ce10e3d643ff45fb9b2372529eea54eb32302c9
Priscilla (Laila Sandra) <a.g.marosi@bluewin.ch>
2025-05-04 21:07:12 +0200

    Impl ∂1_4

--- src/partial.rs
+++ src/partial.rs
@@ -13,9 +13,19 @@ pub fn part1_2(f: fn(Vec<f64>) -> f64, x: Vec<f64>, i: usize, h: f64) -> f64
   return (-0.5*f(a) + 0.5*f(b)) / h;
 }
 
-pub fn part1_4(f: fn(&Vec<f64>) -> f64, mut x_vec: Vec<f64>, i: usize, h: f64) -> f64
+pub fn part1_4(f: fn(&Vec<f64>) -> f64, x: Vec<f64>, i: usize, h: f64) -> f64
 {
-  todo!()
+  let mut a = x.clone();
+  let mut b = x.clone();
+  let mut c = x.clone();
+  let mut d = x.clone();
+
+  a[i] -= 2.*h;
+  b[i] -= h;
+  c[i] += h;
+  d[i] += 2.*h;
+
+  return ((1./12.)*f(&a) - (2./3.)*f(&b) + (2./3.)*f(&c) - (1./12.)*f(&d)) / h;
 }
 
 // ∂²
