a020d1f6c258b04306b4a2df6ad3eebb8b1ed970
Prissilla (Laila Sandra) <a.g.marosi@bluewin.ch>
2026-05-09 14:40:29 +0200

    Better implementation for parallelization.

--- src/integral.rs
+++ src/integral.rs
@@ -4,21 +4,25 @@ use crate::λ;
 // TODO: implement faster methods for calculating integrals.
 
 /// Calculates the integral of `f` between `a` and `b` using
-/// [Simpson's ⅓ rule](https://en.wikipedia.org/wiki/Simpson%27s_rule#Composite_Simpson's_1/3_rule)
+/// [Simpson's 1/3 rule](https://en.wikipedia.org/wiki/Simpson%27s_rule#Composite_Simpson's_1/3_rule)
 /// , so using polynomials of 2ⁿᵈ degree.
-pub fn int_2(a: f64, b: f64, f: fn(f64) -> f64, h: f64) -> f64
+pub fn int_2<Function>(a: f64, b: f64, f: Function, h: f64) -> f64
+where Function: Fn(f64) -> f64
 {
   let n = ((b - a) / h).ceil() as i64;
   let x = λ!{i => a + (i*h)};
 
+  // This partial sum was extracted in hopes that the compiler will make use of SIMD.
+  // Alternatively this is a good way to parallelize it.
   let part = λ!{factor: f64, subtrahend: f64 =>
     (1..=n/2)
       .map(λ!{i => factor * f(x(2.*i as f64 - subtrahend))})
       .sum::<f64>()
   };
 
-  return (1./3.)*h*(part(1., 2.) + part(4., 1.) + part(1., 0.));
+  return (1./3.)*h*(part(1.,2.) + part(4.,1.) + part(1.,0.));
 
+  // NOTE: Old implementation. Do not delete!
   // return (1./3.)*h*(1..=n/2)
   //   .map(|i| i as f64)
   //   .map(|i| f(x(2.*i - 2.)) + 4.*f(x(2.*i - 1.)) + f(x(2.*i)))
@@ -26,58 +30,88 @@ pub fn int_2(a: f64, b: f64, f: fn(f64) -> f64, h: f64) -> f64
 }
 
 /// Calculates the integral of `f` between `a` and `b` using
-/// [Simpson's ⅜ rule](https://en.wikipedia.org/wiki/Simpson%27s_rule#Composite_Simpson's_3/8_rule)
+/// [Simpson's 3/8 rule](https://en.wikipedia.org/wiki/Simpson%27s_rule#Composite_Simpson's_3/8_rule)
 /// , so using polynomials of 3ʳᵈ degree.
-pub fn int_3(a: f64, b: f64, f: fn(f64) -> f64, h: f64) -> f64
+pub fn int_3<Function>(a: f64, b: f64, f: Function, h: f64) -> f64
+where Function: Fn(f64) -> f64
 {
   let mut n = ((b - a) / h).ceil() as i64;
-  let x = |i: f64| a +(i * h);
+  let x = λ!{i => a + (i*h)};
 
   n -= n % 3;
 
   let last_point = x(n as f64); // Casting from int to float is expensive, no? 😬
-  let last_segment = int_2(last_point, b, f, h);
+  let last_segment = int_2(last_point, b, &f, h);
 
-  return (3./8.)*h*(1..=n/3)
-    .map(|i| i as f64)
-    .map(|i| f(x(3.*i - 3.)) + 3.*f(x(3.*i - 2.)) + 3.*f(x(3.*i - 1.)) + f(x(3.*i)))
-    .sum::<f64>() + last_segment;
+  let part = λ!{factor: f64, subtrahend: f64 =>
+    (1..=n/3)
+      .map(λ!{i => factor * f(x(3.*i as f64 - subtrahend))})
+      .sum::<f64>()
+  };
+
+  return (3./8.) * h * (part(1.,3.) + part(3.,2.) + part(3.,1.) + part(1.,0.)) + last_segment;
+
+  // NOTE: Old implementation. Do not delete!
+  // return (3./8.)*h*(1..=n/3)
+  //   .map(|i| i as f64)
+  //   .map(|i| f(x(3.*i - 3.)) + 3.*f(x(3.*i - 2.)) + 3.*f(x(3.*i - 1.)) + f(x(3.*i)))
+  //   .sum::<f64>() + last_segment;
 }
 
 /// Calculates the integral of `f` between `a` and `b` using
 /// [Boole's rule](https://en.wikipedia.org/wiki/Finite_difference_coefficient)
 /// , so using polynomials of 4ᵗʰ degree.
-pub fn int_4(a: f64, b: f64, f: fn(f64) -> f64, h: f64) -> f64
+pub fn int_4<Function>(a: f64, b: f64, f: Function, h: f64) -> f64
+where Function: Fn(f64) -> f64
 {
   let mut n = ((b - a) / h).ceil() as i64;
-  let x = |i: f64| a +(i * h);
+  let x = λ!{i => a + (i*h)};
 
   n -= n % 4;
 
   let last_point = x(n as f64); // Casting from int to float is expensive, no? 😬
-  let last_segment = int_2(last_point, b, f, h);
+  let last_segment = int_2(last_point, b, &f, h);
 
-  return (2./45.)*h*(1..=n/4)
-    .map(|i| i as f64)
-    .map(|i| 7.*f(x(4.*i - 4.)) + 32.*f(x(4.*i - 3.)) + 12.*f(x(4.*i - 2.)) + 32.*f(x(4.*i - 1.)) + 7.*f(x(4.*i)))
-    .sum::<f64>() + last_segment;
+  let part = λ!{factor: f64, subtrahend: f64 =>
+    (1..=n/4)
+      .map(λ!{i => factor * f(x(4.*i as f64 - subtrahend))})
+      .sum::<f64>()
+  };
+
+  return (2./45.) * h * (part(7.,4.) + part(32.,3.) + part(12.,2.) + part(32.,1.) + part(7.,0.)) + last_segment;
+
+  // NOTE: Old implementation. Do not delete!
+  // return (2./45.)*h*(1..=n/4)
+  //   .map(|i| i as f64)
+  //   .map(|i| 7.*f(x(4.*i - 4.)) + 32.*f(x(4.*i - 3.)) + 12.*f(x(4.*i - 2.)) + 32.*f(x(4.*i - 1.)) + 7.*f(x(4.*i)))
+  //   .sum::<f64>() + last_segment;
 }
 
 /// Calculates the integral of `f` between `a` and `b` using
 /// [Weddle's rule](https://mathworld.wolfram.com/WeddlesRule.html)
 /// , so using polynomials of 6ᵗʰ degree.
-pub fn int_6(a: f64, b: f64, f: fn(f64) -> f64, h: f64) -> f64
+pub fn int_6<Function>(a: f64, b: f64, f: Function, h: f64) -> f64
+where Function: Fn(f64) -> f64
 {
   let mut n = ((b - a) / h).ceil() as i64;
-  let x = |i: f64| a +(i * h);
+  let x = λ!{i => a + (i*h)};
 
   n -= n % 6;
 
   let last_point = x(n as f64); // Casting from int to float is expensive, no? 😬
-  let last_segment = int_4(last_point, b, f, h);
+  let last_segment = int_4(last_point, b, &f, h);
 
-  return (3./10.)*h*(1..=n/6)
-    .map(|i| i as f64)
-    .map(|i| f(x(6.*i-6.)) + 5.*f(x(6.*i-5.)) + f(x(6.*i-4.)) + 6.*f(x(6.*i-3.)) + f(x(6.*i-2.)) + 5.*f(x(6.*i-1.)) + f(x(6.*i)))
-    .sum::<f64>() + last_segment;
+  let part = λ!{factor: f64, subtrahend: f64 =>
+    (1..=n/6)
+      .map(λ!{i => factor * f(x(6.*i as f64 - subtrahend))})
+      .sum::<f64>()
+  };
+
+  return (3./10.) * h * (part(1.,6.) + part(5.,5.) + part(1.,4.) + part(6.,3.) + part(1.,2.) + part(5.,1.) + part(1.,0.)) + last_segment;
+
+  // NOTE: Old implementation. Do not delete!
+  // return (3./10.)*h*(1..=n/6)
+  //   .map(|i| i as f64)
+  //   .map(|i| f(x(6.*i-6.)) + 5.*f(x(6.*i-5.)) + f(x(6.*i-4.)) + 6.*f(x(6.*i-3.)) + f(x(6.*i-2.)) + 5.*f(x(6.*i-1.)) + f(x(6.*i)))
+  //   .sum::<f64>() + last_segment;
 }
--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,44 @@
 #![allow(non_upper_case_globals)]
 
 use std::{f64::consts::PI, ops::Neg};
-
 use num_rs::{derivative::{d, d2}, integral::{int_2, int_3, int_4, int_6}, λ};
-
-const h: f64 = 0.01;
-const a: f64 = 1.24;
-const b: f64 = 5.38;
-const π: f64 = PI;
-
-fn F(x: f64) -> f64 { x.sin() }
-fn f(x: f64) -> f64 { x.cos() }
-fn df(x: f64) -> f64 { x.sin().neg() }
-fn d2f(x: f64) -> f64 { x.cos().neg() }
-
 
 fn main()
 {
+  let h = 0.01;
+  let a = 1.24;
+  let b = 5.38;
+  let π = PI;
+
+  let F = λ! {x: f64 => x.sin()};
+  let f = λ! {x: f64 => x.cos()};
+  let df = λ! {x: f64 => x.sin().neg()};
+  let d2f = λ! {x: f64 => x.cos().neg()};
+
   println!("Simpon's 1/3: ε = {:.2e}", (int_2(a, b, f, h) - (F(b) - F(a))).abs());
   println!("Simpon's 3/8: ε = {:.2e}", (int_3(a, b, f, h) - (F(b) - F(a))).abs());
   println!("Boole's :     ε = {:.2e}", (int_4(a, b, f, h) - (F(b) - F(a))).abs());
   println!("Weddle's :    ε = {:.2e}", (int_6(a, b, f, h) - (F(b) - F(a))).abs());
   println!();
 
+  // Area
   println!("Fläche: {:.4}", int_2(a, b, f, h));
+  // Arc length
   println!("Bogenlänge: {:.4}", int_2(a, b, λ!{x => (1. + d(f, x, h).powi(2)).sqrt()}, h));
+  // Rotation(al) volume
   println!("Rotationsvumen: {:.4}", π*int_2(a, b, λ!{x => f(x).powi(2)}, h));
   println!();
 
   println!("d(f): ε = {:.2e}", (df(5.) - d(f, 5., 0.001)).abs());
   println!("d2(f): ε = {:.2e}", (d2f(5.) - d2(f, 5., 0.001)).abs());
+
+  // A nice way to do this.
+  let f = λ!{x: f64 => x.powi(2) + 2.*x + x.cos()};
+  let precision = 0.001;
+  dbg!{format!("{:.5}", d(f, 5., precision))};
+  dbg!{format!("{:.5}", int_2(0., 10., f, precision))};
+  dbg!{format!("{:.5}", int_2(-5., -2., f, precision))};
+  dbg!{format!("{:.5}", int_3(-5., -2., f, precision))};
+  dbg!{format!("{:.5}", int_4(-5., -2., f, precision))};
+  dbg!{format!("{:.5}", int_6(-5., -2., f, precision))};
 }
